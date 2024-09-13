55dc83719e4f44f72268dfb2c365b3128017c1ed
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2024-09-13 04:40:35 -0300

    Case 1: pequenos ajustes

--- candidatura/ProcessoSeletivo.java
+++ candidatura/ProcessoSeletivo.java
@@ -1,2 +1,33 @@
-public class ProcessoSeletivo {
+package candidatura;
+
+public class ProcessoSeletivo
+{
+    // salário usado como base para contratação de candidatos
+    private static final double SALARIO_BASE = 2000.00;
+
+    public static void main(String[] args) {
+        System.out.println("Processo seletivo");
+        analisarCandidato(1900.00);
+        analisarCandidato(2200.00);
+        analisarCandidato(2000.00);
+    }
+
+    /**
+     * Analisa candidatos com base em suas propostas salariais.
+     * @param salarioPretendido
+     */
+    public static void analisarCandidato(double salarioPretendido)
+    {
+        if (SALARIO_BASE > salarioPretendido)
+        {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        } else if (SALARIO_BASE == salarioPretendido)
+        {
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTAR-PROPOSTA");
+        } else
+        {
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
+
 }
