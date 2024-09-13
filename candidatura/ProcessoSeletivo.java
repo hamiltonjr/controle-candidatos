fc3e762417cc88f38cd5122c7cf802eba290b360
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2024-09-13 05:04:39 -0300

    Case 2: inclusão do método selecionarCandidatos() e teste no método principal

--- candidatura/ProcessoSeletivo.java
+++ candidatura/ProcessoSeletivo.java
@@ -1,15 +1,31 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo
 {
     // salário usado como base para contratação de candidatos
     private static final double SALARIO_BASE = 2000.00;
 
+    // total de candidatos selecionados
+    private static final int TOTAL_CANDIDATOS = 5;
+
+    // lista de candidatos
+    private static final String[] CANDIDATOS = {
+            "FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO",
+            "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"
+    };
+
     public static void main(String[] args) {
         System.out.println("Processo seletivo");
-        analisarCandidato(1900.00);
-        analisarCandidato(2200.00);
-        analisarCandidato(2000.00);
+        // testes do case 1 comentados
+        //analisarCandidato(1900.00);
+        //analisarCandidato(2200.00);
+        //analisarCandidato(2000.00);
+
+        // teste do case 2
+        selecionarCandidatos();
+
     }
 
     /**
@@ -28,6 +44,44 @@ public class ProcessoSeletivo
         {
             System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
         }
+    }
+
+    /**
+     * Seleciona certa quantidade de candidatos com base em propostas
+     * salariais dos mesmos.
+     */
+    public static void selecionarCandidatos()
+    {
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+
+        while (candidatosSelecionados < TOTAL_CANDIDATOS &&
+            candidatoAtual < CANDIDATOS.length)
+        {
+            String candidato = CANDIDATOS[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato +
+                    " solicitou este valor de salário: " +
+                    String.format("%1.2f", salarioPretendido));
+            if (SALARIO_BASE >= salarioPretendido)
+            {
+                System.out.println("O candidato " + candidato +
+                        " foi selecionado para a vaga.");
+                candidatosSelecionados++;
+            }
+            System.out.println();
+            candidatoAtual++;
+        }
+    }
+
+    /**
+     * Simula a proposição de salário dos candidatos.
+     * @return proposição aleatória de salário
+     */
+    private static double valorPretendido()
+    {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
 }
